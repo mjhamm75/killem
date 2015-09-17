b947d5d14b3ab35c81ef516d05be84ea2822d09e
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-17 06:45:40 -0600

    Change bootstrap grid to flexbox

--- app/js/components/login.js
+++ app/js/components/login.js
@@ -8,14 +8,12 @@ var Login = React.createClass({
 
 	render: function() {
 		return (	
-			<div className="row">
-				<div className="col-md-2 col-md-offset-5 col-sm-2 col-sm-offset-5 col-xs-2 col-xs-offset-5 vertical-center">
-					<form>
-						<div className="form-group">
-							<button type="button" className="btn btn-success" onClick={this._handleClick}>Log in to Spotify</button>
-						</div>
-					</form>
-				</div>
+			<div className="flex-container">
+				<form>
+					<div className="form-group">
+						<button type="button" className="btn btn-success" onClick={this._handleClick}>Log in to Spotify</button>
+					</div>
+				</form>
 			</div>
 		);
 	}
