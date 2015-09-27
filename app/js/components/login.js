536e58fde903f463fb7f1858264bfea835618714
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-26 22:11:25 -0600

    Change to ES6 class syntax

--- app/js/components/login.js
+++ app/js/components/login.js
@@ -1,22 +1,17 @@
-var React = require('react');
-var AppActions = require('./../actions/app.actions.js');
+import React from 'react';
 
-var Login = React.createClass({
-	_handleClick: function() {
-		AppActions.login();
-	},
-
-	render: function() {
+class Login extends React.Component {
+	render() {
 		return (	
 			<div className="flex-container">
 				<form>
 					<div className="form-group">
-						<button type="button" className="btn btn-success" onClick={this._handleClick}>Log in to Spotify</button>
+						<button type="button" className="btn btn-success" onClick={this.props.handleLogin}>Log in to Spotify</button>
 					</div>
 				</form>
 			</div>
 		);
 	}
-});
+};
 
-module.exports = Login;
+export default Login;
--- app/js/router.js
+++ app/js/router.js
@@ -1,19 +1,28 @@
+// require('./../sass/app.scss');
+
 import React from 'react';
 import { Router, Route } from 'react-router';
+import { Provider } from 'react-redux';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import configureStore from './stores/redux.store.js';
 
-require('./stores/app.store.js');
-
-var App = require('./components/login.js');
+var LoginContainer = require('./containers/LoginContainer.js');
 var CreatePlaylist = require('./components/create.js');
 var Juking = require('./components/juking.js');
 var About = require('./components/about.js');
 
+const store = configureStore();
+
 React.render(
-	<Router>
-		<Route path="/" component={App} />
-		<Route path="/create" component={CreatePlaylist} />
-		<Route path="/juking" component={Juking} />
-		<Route path="/about" component={About} />
-	</Router>,
+	<Provider store={store}>
+		{() =>
+			<Router>
+				<Route path="/" component={LoginContainer} />
+				<Route path="/create" component={CreatePlaylist} />
+				<Route path="/juking" component={Juking} />
+				<Route path="/about" component={About} />
+			</Router>
+		}
+	</Provider>,
 	document.getElementById('example')
 );
