218d56e85072edbd47ed35cd2cbb4af123291829
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-29 20:30:25 -0600

    Use edge version of babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 			{
                 test: /\.js?$/,
                 exclude: /(node_modules)/,
-                loader: 'babel'
+                loader: 'babel?optional[]=runtime&stage=0'
             }
 		]
 	}
