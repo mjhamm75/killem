031344af424238bbef801b2f7e98437beacaedab
Jason Hamm <jasonhamm.me@gmail.com>
2015-10-18 15:47:08 -1000

    Add route to Create playlist

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from './../containers/App';
-import Login from './../containers/LoginPage';
 import About from './../containers/About';
+import Create from './../containers/Create';
+import Login from './../containers/LoginPage';
 
 export default (
   <Route path="/" component={App}>
   	<Route path="/login" component={Login} />
   	<Route path="/about" component={About} />
+  	<Route path="/create" component={Create} />
   </Route>
 );
