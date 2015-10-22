35d97e95fc7d2ee385f642a893dad6be5f4042a8
Jason Hamm <jasonhamm.me@gmail.com>
2015-10-22 09:11:56 -0600

    Update actions to create playlist

--- actions/killem.actions.js
+++ actions/killem.actions.js
@@ -5,7 +5,9 @@ export function login(history) {
 	return dispatch => {
 		dispatch(loginRequest());
 		return isoFetch('/log-in')
-			.then(response => dispatch(loginSuccessful(history, response.url)))
+			.then(response => {
+				dispatch(loginSuccessful(history, response.url));
+			})
 	}
 }
 
@@ -26,8 +28,8 @@ function loginSuccessful(history, url) {
 export function getMe() {
 	return dispatch => {
 		dispatch(getMeRequest());
-		return isoFetch('/me').then(me => {
-			dispatch(getMeSuccess(me))
+		return isoFetch('/me').then(response => {
+			dispatch(getMeSuccess(response.body))
 		})		
 	}
 }
@@ -42,5 +44,60 @@ function getMeSuccess(me) {
 	return {
 		type: AppConstants.GET_ME_SUCCESS,
 		me
+	}
+}
+
+export function getTokens() {
+	return dispatch => {
+		dispatch(getTokensRequest())
+		return isoFetch('/tokens')
+			.then(tokens => {
+				dispatch(getTokensSuccess(tokens))
+			});
+	}
+}
+
+function getTokensRequest() {
+	return {
+		type: AppConstants.GET_TOKENS_REQUEST
+	}
+}
+
+function getTokensSuccess(tokens) {
+	return {
+		type: AppConstants.GET_TOKENS_SUCCESS,
+		tokens
+	}
+}
+
+export function createPlaylist() {
+	return (dispatch, getState) => {
+		var playlist = {
+			name: 'CrackList',
+			public: true
+		};
+		dispatch(createPlaylistRequest());
+		return isoFetch('https://api.spotify.com/v1/users/' + getState().me.id + '/playlists', {
+			method: 'post',
+			headers: {
+				'Authorization': 'Bearer ' + getState().tokens.access_token,
+				'Content-Type': 'application/json'	
+			},
+			body: JSON.stringify(playlist)
+		})
+		.then(playlist => dispatch(createPlaylistSuccessful(playlist)));
+	}
+}
+
+function createPlaylistRequest() {
+	return {
+		type: AppConstants.CREATE_PLAYLIST_REQUEST
+	}
+}
+
+function createPlaylistSuccessful(playlist) {
+	return {
+		type: AppConstants.CREATE_PLAYLIST_SUCCESSFUL,
+		playlist
 	}
 }
