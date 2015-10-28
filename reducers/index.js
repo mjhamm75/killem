c9f4e2a4386db6444a836cbf97417440f14e96da
Jason Hamm <jasonhamm.me@gmail.com>
2015-10-27 21:53:32 -0600

    Remove old code

--- reducers/index.js
+++ reducers/index.js
@@ -1,18 +1,7 @@
 import * as ActionTypes from '../actions';
-import merge from 'lodash/object/merge';
-import paginate from './paginate';
 import { routerStateReducer as router } from 'redux-router';
 import { combineReducers } from 'redux';
 import { GET_ME_SUCCESS, GET_TOKENS_SUCCESS } from './../constants/killem.constants.js';
-
-// Updates an entity cache in response to any action with response.entities.
-function entities(state = { users: {}, repos: {} }, action) {
-  if (action.response && action.response.entities) {
-    return merge({}, state, action.response.entities);
-  }
-
-  return state;
-}
 
 // Updates error message to notify about the failed fetches.
 function errorMessage(state = null, action) {
@@ -45,29 +34,7 @@ function tokens(state = {}, action) {
   }
 }
 
-// Updates the pagination data for different actions.
-const pagination = combineReducers({
-  starredByUser: paginate({
-    mapActionToKey: action => action.login,
-    types: [
-      ActionTypes.STARRED_REQUEST,
-      ActionTypes.STARRED_SUCCESS,
-      ActionTypes.STARRED_FAILURE
-    ]
-  }),
-  stargazersByRepo: paginate({
-    mapActionToKey: action => action.fullName,
-    types: [
-      ActionTypes.STARGAZERS_REQUEST,
-      ActionTypes.STARGAZERS_SUCCESS,
-      ActionTypes.STARGAZERS_FAILURE
-    ]
-  })
-});
-
 const rootReducer = combineReducers({
-  entities,
-  pagination,
   errorMessage,
   me,
   router,
