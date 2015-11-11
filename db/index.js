9a60e50cc1d8bcc15185a4f757841bc22c58ca1c
Jason Hamm <jasonhamm.me@gmail.com>
2015-11-10 22:18:19 -0700

    Move login function to db

--- db/index.js
+++ db/index.js
@@ -5,3 +5,21 @@ export function searchTracks(term) {
 	console.log(url);
 	return axios.get(url);
 }
+
+export function login() {
+	var state = generateRandomString(16);
+	res.cookie(stateKey, state);
+
+	var scope = 'user-read-private user-read-email playlist-modify-public';
+	var result = 'https://accounts.spotify.com/authorize?' +
+	querystring.stringify({
+		response_type: 'code',
+		client_id: client_id,
+		scope: scope,
+		redirect_uri: redirect_uri,
+		state: state
+	});
+	res.json({
+		url: result
+	});
+}
