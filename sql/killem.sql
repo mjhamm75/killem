b4241578e1c4f4816398c0e30cc3cb9cda7a7209
Jason Hamm <jasonhamm.me@gmail.com>
2015-11-28 22:45:31 -0700

    Add second table for playlists

--- sql/killem.sql
+++ sql/killem.sql
@@ -1,9 +1,10 @@
 create database killem;
 
-create table users (id serial, user_name varchar(50), access_token varchar, refresh_token varchar);
+create table users (id serial not null primary key, user_name varchar(50), access_token varchar, refresh_token varchar);
 
 GRANT ALL PRIVILEGES ON TABLE users TO killem;
+GRANT ALL PRIVILEGES ON SEQUENCE users_id_seq TO killem;
 GRANT USAGE, SELECT ON SEQUENCE users_id_seq TO killem;
 
 
-create table playlists (id serial, playlist_id varchar(100), playlist_name varchar(50), active_playlist boolean, user_id integer references users(id));
+create table playlists (id serial not null primary key, playlist_id varchar(100), playlist_name varchar(50), active_playlist boolean, user_id integer references users(id));
