9aefaebcd420227569d1d486fdf2c67bfa5b9e1e
Jason Hamm <jasonhamm.me@gmail.com>
2015-12-03 17:59:28 -0700

    Change to stateless component format

--- components/Login.js
+++ components/Login.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-class Login extends React.Component {
-	render() {
-		return (	
-			<div className="flex-container">
-				<form>
-					<div className="form-group">
-						<button type="button" className="btn btn-success" onClick={this.props.login}>Log in to Spotify</button>
-					</div>
-				</form>
+const Login = ({login}) => (
+	<div className="flex-container">
+		<form>
+			<div className="form-group">
+				<button type="button" className="btn btn-success" onClick={login}>Log in to Spotify</button>
 			</div>
-		);
-	}
-};
+		</form>
+	</div>
+)
 
 export default Login;
