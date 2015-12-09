1a5f94cb8f3dc0b34cd0ade53d29e88a55dd19b5
Jason Hamm <jasonhamm.me@gmail.com>
2015-12-09 08:31:15 -0700

    Update onClick to be created with prop

--- components/Track.js
+++ components/Track.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Track = ({addTrack, track}) => (
-	<div onClick={addTrack(track.id)}>{track.name}</div>
+	<div onClick={() => addTrack(track.id)}>{track.name}</div>
 )
 
 export default Track;
