25e825a27cd5f87c4beff70b6b88d39a1c4e4d4f
Jason Hamm <jasonhamm.me@gmail.com>
2015-12-14 17:29:54 -0700

    Add debug flag

--- config/index.js
+++ config/index.js
@@ -9,6 +9,7 @@ export const PORT = 8888;
 export function knexInit() {
 	return require('knex')({
 	    client: 'pg',
-	    connection: 'postgres://killem:killem@localhost/killem'
+	    connection: 'postgres://killem:killem@localhost/killem',
+	    debug: false
 	});	
 }
